288db048409dd53374091feee3ed386817832dfe
Igor Davydenko <playpauseandstop@gmail.com>
2009-04-23 14:35:31 +0300

    Decorators from ``nose.tools`` (``make_decorator``, ``raises``, ``timed``, ``with_setup``) now available as ``NoseTestCase`` classmethods.

--- tddspry/cases.py
+++ tddspry/cases.py
@@ -4,9 +4,29 @@ from nose import tools
 __all__ = ('NoseTestCase', )
 
 
-class NoseTestCase(object):
+class TestCaseMetaclass(type):
 
-    def __init__(self, *args, **kwargs):
-        super(NoseTestCase, self).__init__(*args, **kwargs)
+    def __new__(cls, name, bases, attrs):
+        def func(attr):
+            return lambda _, *args, **kwargs: \
+                       getattr(tools, attr)(*args, **kwargs)
+
+        decorators = ('make_decorator', 'raises', 'timed', 'with_setup')
+
         for attr in tools.__all__:
-            setattr(self, attr, getattr(tools, attr))
+            if attr in decorators:
+                setattr(cls, attr, func(attr))
+            else:
+                attrs.update({attr: func(attr)})
+
+        return type.__new__(cls, name, bases, attrs)
+
+
+class BaseTestCase(object):
+
+    pass
+
+
+class NoseTestCase(BaseTestCase):
+
+    __metaclass__ = TestCaseMetaclass
