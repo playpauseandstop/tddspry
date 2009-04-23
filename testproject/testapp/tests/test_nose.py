4b00b2cee87d798235926d6f5eb82fb105e3179a
Igor Davydenko <playpauseandstop@gmail.com>
2009-04-23 14:23:51 +0300

    More testing for ``tddspry.NoseTestCase``.

--- testproject/testapp/tests/test_nose.py
+++ testproject/testapp/tests/test_nose.py
@@ -1,4 +1,14 @@
+import time
+
 from tddspry import NoseTestCase
+
+
+def dummy_setup():
+    pass
+
+
+def dummy_teardown():
+    pass
 
 
 def raise_exception():
@@ -7,7 +17,7 @@ def raise_exception():
 
 class TestNose(NoseTestCase):
 
-    def test_nose_tools(self):
+    def test_methods(self):
         self.ok_(True)
         self.eq_(True, True)
 
@@ -24,3 +34,39 @@ class TestNose(NoseTestCase):
         self.assert_false(False)
         self.assert_raises(TypeError, raise_exception)
         self.assert_true(True)
+
+    @NoseTestCase.raises(TypeError)
+    def test_raises(self):
+        raise_exception()
+
+    @NoseTestCase.raises(AssertionError, TypeError, ValueError)
+    def test_raises_complex(self):
+        self.ok_(False)
+        raise_exception()
+        raise ValueError
+
+    @NoseTestCase.raises(TypeError)
+    @NoseTestCase.raises(ValueError)
+    def test_raises_failed(self):
+        raise_exception()
+
+    @NoseTestCase.timed(10)
+    def test_timed(self):
+        pass
+
+    @NoseTestCase.raises(AssertionError)
+    @NoseTestCase.timed(1)
+    def test_timed_failed(self):
+        time.sleep(2)
+
+    @NoseTestCase.with_setup(dummy_setup, dummy_teardown)
+    def test_with_setup_complex(self):
+        pass
+
+    @NoseTestCase.with_setup(setup=dummy_setup)
+    def test_with_setup_setup(self):
+        pass
+
+    @NoseTestCase.with_setup(teardown=dummy_teardown)
+    def test_with_setup_teardown(self):
+        pass
