c9cbc754a321243593e9e47aac5f05f728b6dd68
Igor Davydenko <playpauseandstop@gmail.com>
2010-04-06 03:56:26 +0300

    #18: Bump version number. Fix problem with installing in isolated environments.

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
-
 import os
-from setuptools import setup
+
+from distutils.core import setup
 
 
 readme = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
 README = readme.read()
 readme.close()
 
-requires = ['Django (>= 1.0)', 'mock (>= 0.5.0)', 'nose (>= 0.10.3)',
-            'twill (>= 0.9)']
+requires = ['Django (>= 1.0)', 'nose (>= 0.10.3)', 'twill (>= 0.9)']
 version = __import__('tddspry').get_version()
 
 setup(name='tddspry',
@@ -22,10 +21,9 @@ setup(name='tddspry',
       author_email='talk@42coffeecups.com',
       maintainer='Igor Davydenko',
       maintainer_email='playpauseandstop@gmail.com',
-      url='http://github.com/playpauseandstop/tddspry',
+      url='http://github.com/42/tddspry',
 
       packages=['tddspry', 'tddspry.django', 'tddspry.django.helpers'],
-      package_data={'': ['*.rst']},
       scripts=['bin/django-nosetests.py'],
 
       classifiers=[
@@ -35,8 +33,6 @@ setup(name='tddspry',
           'License :: OSI Approved :: GNU General Public License (GPL)',
           'Topic :: Software Development :: Testing',
       ],
-      keywords='django mock nose tdd testing tests twill',
+      keywords='django nose tdd testing tests twill',
 
-      install_requires=[require.replace('(', '').replace(')', '') \
-                        for require in requires],
       requires=requires)
--- tddspry/__init__.py
+++ tddspry/__init__.py
@@ -5,26 +5,24 @@
    :members:
 
 """
-
 from tddspry.cases import *
 
 
-VERSION = (0, 3, 2, 'alpha')
+VERSION = (0, 4, 'alpha')
 
 
 def get_version():
     """
     Returns human-readable version of your **tddspry** installation.
     """
-    def intjoin(data, sep):
-        return sep.join(str(i) for i in data)
+    intjoin = lambda data, sep=None: (sep or '.').join(map(str, data))
 
     if VERSION[-1] is not None:
         if isinstance(VERSION[-1], int):
-            version = intjoin(VERSION, '.')
+            version = intjoin(VERSION)
         else:
-            version = '%s_%s' % (intjoin(VERSION[:-1], '.'), VERSION[-1])
+            version = '%s_%s' % (intjoin(VERSION[:-1]), VERSION[-1])
     else:
-        version = intjoin(VERSION[:-1], '.')
+        version = intjoin(VERSION[:-1])
 
     return version
