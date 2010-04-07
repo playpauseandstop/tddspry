dc9837357237263e154cca56e2d3a5bdf75bd9dd
Igor Davydenko <playpauseandstop@gmail.com>
2010-04-08 02:48:40 +0300

    #20: Fix warning in ``django-nosetests.py`` script.

--- bin/django-nosetests.py
+++ bin/django-nosetests.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 """
 Custom ``nosetests`` runner that make possible to run ``nosetests`` for your
-Django projects.
+Django projects without installed ``setuptools``.
 """
 
 import nose
 
-from tddspry.noseplugins import DjangoPlugin
-
 
 if __name__ == '__main__':
-    nose.main(addplugins=[DjangoPlugin()])
+    kwargs = {}
+
+    try:
+        import setuptools
+    except ImportError:
+        from tddspry.noseplugins import DjangoPlugin
+        kwargs = {'addplugins': [DjangoPlugin()]}
+
+    nose.main(**kwargs)
