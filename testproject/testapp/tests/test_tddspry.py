b3f751073214a4fd63f6388bd4a88be52fa46c1c
Igor Davydenko <playpauseandstop@gmail.com>
2010-10-11 17:21:18 +0300

    #46: Add tests to future ``find_in`` and ``notfind_in`` methods of base ``tddspry.TestCase`` class.

--- testproject/testapp/tests/test_tddspry.py
+++ testproject/testapp/tests/test_tddspry.py
@@ -1,7 +1,9 @@
+
+import re
+
 import tddspry
 
-from tddspry import TestCase
-from tddspry.utils import camelcase_to_underscore, underscore_to_camelcase
+from tddspry.utils import *
 
 
 TEST_METHOD_NAMES = (
@@ -22,6 +24,23 @@ TEST_METHOD_NAMES = (
     ('assert_update', 'assertUpdate'),
 )
 
+TEST_RE_FLAGS = (
+    (None, 0),
+    ('i', re.I),
+    ('u', re.U),
+    ('c', 0),
+    ('I', re.I),
+    ('U', re.U),
+    ('C', 0),
+    ('iu', re.I | re.U),
+    ('Iu', re.I | re.U),
+    ('IU', re.I | re.U),
+    ('iU', re.I | re.U),
+    ('ilmsux', re.I | re.L | re.M | re.S | re.U | re.X),
+    ('ILMSUX', re.I | re.L | re.M | re.S | re.U | re.X),
+    ('ialbmcsduex', re.I | re.L | re.M | re.S | re.U | re.X),
+)
+
 TEST_VERSIONS = (
     ((0, 1), '0.1'),
     ((0, 1, None), '0.1'),
@@ -38,6 +57,10 @@ class TestUtils(tddspry.TestCase):
     def test_camelcase_to_underscore(self):
         for underscore, camelcase in TEST_METHOD_NAMES:
             self.assert_equal(camelcase_to_underscore(camelcase), underscore)
+
+    def test_process_re_flags(self):
+        for flags, result in TEST_RE_FLAGS:
+            self.assert_equal(process_re_flags(flags), result)
 
     def test_underscore_to_camelcase(self):
         for underscore, camelcase in TEST_METHOD_NAMES:
